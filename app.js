4b09f2671eb0894283f607130cf88bca7270735e
kifeb <rizkyfebrianto947@gmail.com>
2023-01-13 09:18:58 +0700

    make it better

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/UserRoute.js"
 import bodyParser from "body-parser";
 
+//Konfigurasi file .env
 dotenv.config();
+
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use('/assets', express.static('public/images'))
 
+// Routes for user
 app.use("/user", userRoutes);
 
 app.use((err, req, res, next) => {
